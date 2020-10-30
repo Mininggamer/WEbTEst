0158dc5f5e6860f93ecb2dc261de1e6014325017
Nghĩa Phạm <thanhariko@gmail.com>
2020-10-30 17:35:27 +0700

    hi

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const server = express()
 const wss = new Server({ server });
 wss.on('connection', (ws) => {
     console.log('Client connected');
-    ws.on('close', () => console.log('Client disconnected'));
+    wss.on('close', () => console.log('Client disconnected'));
   });
 
 
